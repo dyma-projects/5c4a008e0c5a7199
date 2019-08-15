20433770fa25684b48ff13c1e8a3b64f76aac9a4
JuliusMaximus <contact@weckjulien.fr>
2019-08-15 21:00:31 +0200

    exercice fini ;)

--- src/app/exercice-1/color.directive.ts
+++ src/app/exercice-1/color.directive.ts
@@ -1,8 +1,28 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({ selector: '[changeTextColor]' })
 export class ColorDirective {
-  constructor(el: ElementRef) {
-    el.nativeElement.style.color = 'red';
-  }
+
+    constructor(private el: ElementRef) {} 
+    
+    @HostListener('window:keydown', ['$event']) windowKey($event) {
+        switch ($event.keyCode) {
+            case 37:
+                this.changeColor('green');
+                break;
+            case 38:
+                this.changeColor('orange');
+                break;
+            case 39:
+                this.changeColor('red');
+                break;
+            case 40:
+                this.changeColor('blue');
+                break;
+        }
+    }
+
+    private changeColor(color: string) {
+        this.el.nativeElement.style.color = color;
+    }
 }
